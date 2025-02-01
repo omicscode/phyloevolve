187cc3544b9a7b626bad178374b29d0e8c89d99e
Gaurav Sablok <gauravcodepro@gmail.com>
2025-02-01 23:11:16 +0100

    sciencegenome
    started coding this graphtools and all neovim including plugs, languageserver and everything, treesitter removed from my setup, Switched to Helix completely.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/*
+ Author: Gaurav Sablok
+ SLB Potsdam
+ Date 2025-1-2
+
+ graphtools - a set of the collection of the
+ subtools for the graph alignment.
+*/
+
 fn main() {
     println!("Hello, world!");
 }
