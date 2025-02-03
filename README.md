8fcf2c369912b6d9487be35ce3effd96a9778aa3
Gaurav Sablok <gauravcodepro@gmail.com>
2025-02-03 23:03:47 +0100

    sciencegenome
    first binary build, updated the collinearity block algorithm and writing the code for the graph linearization to bandage alignment viewer.  Following adding protein sequences

--- README.md
+++ README.md
@@ -2,5 +2,28 @@
  - rust based graphtools for genome, metagenome, pangenome.
  - coded all the parts of this and two block collinearity algorithms for block wise comparison. 
  - please see the last commit message and if it says compiled binary then it is completed or else still in development version. 
- 
+
+ ```
+ cargo build 
+ ```
+ ```
+ ➜  graphtools git:(main) ✗ ./target/debug/graphtools -h
+ all graph and alignments tools
+
+ Usage: graphtools <COMMAND>
+
+ Commands:
+  alignmerge          merge all the alignment into a single string
+  alignmergeinterval  alignmerge the specific region of the alignment
+  same-alignment      remove same alignment
+  alignmentstats      alignmentstats
+  filter-site         filter the sites with the given base
+  filter-all          removes same bases across all the alignment
+  filter-block        collineratiy block based alignment filtering
+  help                Print this message or the help of the given subcommand(s)
+
+ Options:
+  -h, --help     Print help
+  -V, --version  Print version
+ ``` 
  Gaurav Sablok
--- src/filterblock.rs
+++ src/filterblock.rs
@@ -25,56 +25,53 @@ pub fn filterblockalignment(path: &str, block: &str) -> Result<String, Box<dyn E
         }
     }
 
-    let blockinner = block.parse::<usize>().wrap();
-
-    let mut finalvecheader: Vec<_> = Vec::new();
-    let mut finalvecseq: Vec<_> = Vec::new();
+    let blockinner = block.parse::<usize>().unwrap();
+    let mut finalvecseq: Vec<Vec<String>> = Vec::new();
 
     /*
     implementing the block collineraity score here
     */
 
-    for i in 0..finalvecseq.len() - 1 {
-        let seqi: String = finalvecseq[i];
-        let seqinext: String = finalvecseq[i + 1];
-        let windowedseqi: Vec<_> = seqi
-            .chars()
-            .collect::<Vec<_>>()
-            .into_iter()
-            .windows(block)
-            .collect::<Vec<_>>();
-        let windowseqinext: Vec<_> = seq[i + 1]
-            .chars()
-            .collect::<Vec<_>>()
-            .into_iter()
-            .window(block)
-            .collect::<Vec<_>>();
-
+    for i in 0..seq.len() - 1 {
         let mut score1: usize = 0usize;
         let mut score2: usize = 0usize;
 
         let mut refinedfilter1: Vec<_> = Vec::new();
         let mut refinedfilter2: Vec<_> = Vec::new();
 
-        for i in windowedseqi.iter() {
-            for j in windowseqinext.iter() {
-                for count1 in i.iter() {
-                    for count2 in j.iter() {
+        for seq in seq[i]
+            .chars()
+            .map(|x| String::from(x))
+            .collect::<Vec<_>>()
+            .windows(blockinner)
+            .collect::<Vec<_>>()
+            .into_iter()
+        {
+            for seqiter in seq[i + 1]
+                .chars()
+                .map(|x| String::from(x))
+                .collect::<Vec<_>>()
+                .windows(blockinner)
+                .collect::<Vec<_>>()
+                .into_iter()
+            {
+                for count1 in seq.iter() {
+                    for count2 in seqiter.iter() {
                         if count1 == count2 {
                             score1 += 1
                         } else if count1 != count2 {
                             score2 += 1
                         }
-                        if score2 > score1 {
+                        if score2 < score1 {
                             continue;
                         } else if score2 > score1 {
-                            refinedfilter1.push(i);
-                            refinedfilter2.push(j);
+                            refinedfilter1.push(seq.join("").to_string());
+                            refinedfilter2.push(seqiter.join("").to_string());
                         }
                     }
                 }
-                finalvecseq.push(refinedfilter1);
-                finalvecseq.push(refinedfilter2);
+                finalvecseq.push(refinedfilter1.clone());
+                finalvecseq.push(refinedfilter2.clone());
             }
         }
     }
